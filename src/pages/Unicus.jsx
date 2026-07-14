597c2b857e2b02a749c730baccc792b497054fad
aes-gems-edu-in <240846926+aes-gems-edu-in@users.noreply.github.com>
2026-07-15 00:10:47 +0530

    video chnaged for mobile

--- src/pages/Unicus.jsx
+++ src/pages/Unicus.jsx
@@ -35,12 +35,13 @@ const sectionIds = [
 
 // Website Data
 export const websiteData = {
+  // ffmpeg -i heroVideo.mp4 -c copy -movflags +faststart output.mp4
   // ffmpeg -i your-video.mp4 -c:v libx264 -g 1 -keyint_min 1 -crf 18 -preset ultrafast output.mp4
   // ffmpeg -i your-video.mp4 -c:v libx264 -g 1 -keyint_min 1 -crf 23 -preset fast -vf "scale=720:1280" -movflags +faststart mobile-output.mp4 && \
   // ffmpeg -i output.mp4 -vframes 1 -q:v 2 hero-poster.jpg
   hero: {
     video: "/img/output4.mp4",
-    videoMobile: "/img/hero-mobile.mp4",
+    videoMobile: "/img/output4.mp4",
     videoWebm: "/img/hero.webm",
     poster: "/img/hero-poster.jpg", 
     title: "Clean Spaces",
